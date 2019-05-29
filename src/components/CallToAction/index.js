656e6c57681590ceee14ab5cb401429845e152ff
mdrae <mdrae0@gmail.com>
2019-05-28 19:49:10 -0500

    Separates CallToAction from LandingPage
    This commit makes `CallToAction` its own component and calls it on
    `LandingPage`.
    
    This commit also adds a mobile design to the `LandingPage`.
    
    Lastly, this commit adds a fade-in animation to `CallToAction` when the
    page is entered.

---
+++ src/components/CallToAction/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import * as cta from './calltoaction.module.scss'
+import Container from '../../components/Container'
+import Row from '../../components/Row'
+import { H2, H3 } from '../../components/Typography'
+
+class CallToAction extends React.Component {
+  render () {
+    return (
+      <div className={cta.component} >
+        <div className={cta.image__container}>
+          <img src="https://img.icons8.com/dotty/80/000000/copybook.png" />
+        </div>
+        <H2 className={cta.text__title} bold
+          text={'Create a Personal Todo List'} />
+        <H3 className={cta.text__subtitle}
+          text={'Plan your day, track errands, and keep your life organized.'} />
+        <Link className={cta.btn} to='/notes'>
+          <H3 text={'Get Started'} />
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default CallToAction;
--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom'
 import * as landing from './landing.module.scss'
 import Container from '../../components/Container'
 import Row from '../../components/Row'
-import { H1, H2, H3, H4 , H5, H6 } from '../../components/Typography'
-import { RaceOperator } from 'rxjs/internal/observable/race';
+import CallToAction from '../../components/CallToAction'
 
 class LandingPage extends React.Component {
   render () {
@@ -14,18 +13,7 @@ class LandingPage extends React.Component {
         <Container className={landing.container}>
           <Row className={landing.row}>
 
-            <div className={landing.component} >
-              <div className={landing.image__container}>
-                <img src="https://img.icons8.com/dotty/80/000000/copybook.png" />
-              </div>
-              <H2 className={landing.text__title} bold
-                text={'Create a Personal Todo List'} />
-              <H3 className={landing.text__subtitle}
-                text={'Plan your day, track errands, and keep your life organized.'} />
-              <Link className={landing.btn} to='/notes'>
-                <H3 text={'Get Started'} />
-              </Link>
-            </div>
+            <CallToAction />
 
           </Row>
         </Container>
