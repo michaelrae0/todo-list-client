6b24a4fe46509af7b9454b20614a30e110a793a2
mdrae <mdrae0@gmail.com>
2019-05-29 13:23:19 -0500

    Adds animations to task completion
    This commit adds `slideOut` and `slideUp` animations to `Tasks` to give
    the illusion of deleting the task from the list.
    
    This commit also adds borders between the tasks.
    
    Lastly, this commit lowers the font-sizes of the `TaskList`.

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,21 +1,48 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import * as task from './task.module.scss'
 import Row from '../Row'
-import { H2 } from '../Typography'
 import { ReactComponent as CheckMarkSVG } from '../../images/checkmark-black.svg'
 
+//input is h3
 class Task extends React.Component {
-  render () {
-    const { name } = this.props.task;
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      value: this.props.task.name,
+      slideOut: false,
+    }
+  }
+
+  handleCheck = () => {
+    const { index, handleClick } = this.props
+
+    this.setState({ slideOut: true })
+    setTimeout(() => handleClick(index), 550)
+  }
+
+  render () {
+    const { bordered, slideUp } = this.props
+    const { value, slideOut } = this.state
+
+    // wrapper handles opacity transform separately from row's translateX
     return (
-      <Row className={task.row} noMargin>
-        <div className={task.status} >
-          <CheckMarkSVG className={task.status__checkmark}/>
-        </div>
-        <H2 className={task.title} text={name} />
-      </Row>
+      <div className={classnames(task.wrapper, {[task.wrapper__slideOut]: slideOut}, {[task.wrapper__slideUp]: slideUp})}>
+        <Row className={classnames(task.row, {[task.row__slideOut]: slideOut})} noMargin>
+          <div className={task.status} onClick={this.handleCheck} >
+            <CheckMarkSVG className={task.status__checkmark}/>
+          </div>
+          <input 
+            type='text' placeholder='Enter a Task'
+            className={task.input}
+            value={value}
+            onChange={e => this.setState({ value: e.target.value })}
+          />
+        </Row>
+        {bordered && <div className={task.border}/>}
+      </div>
     )
   }
 }
