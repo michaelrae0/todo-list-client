8abcff2e52708f544ec6e0fae3db73611e254a25
mdrae <mdrae0@gmail.com>
2019-05-30 01:28:45 -0500

    Adds handle for dragging

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -29,13 +29,22 @@ class Task extends React.Component {
     const { task, index, bordered, slideUp, fadeBorder } = this.props
     const { value, slideOut } = this.state
 
+    const dragHandle = (provided) => { // self executes
+      let arr = Array(6).fill('')
+      return (
+        <div className={taskStyles.handle} {...provided.dragHandleProps}>
+          {arr.map(() => <div className={taskStyles.handle__dimple} />)}
+        </div>
+      )
+    }
+
     // wrapper handles opacity transform separately from row's translateX
     return (
       <Draggable draggableId={task.id} index={index}>
         {(provided, snapshot) => (
 
           <div 
-            {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}
+            {...provided.draggableProps} ref={provided.innerRef}
             className={classnames(
               taskStyles.wrapper, 
               {[taskStyles.wrapper__slideOut]: slideOut}, 
@@ -44,6 +53,7 @@ class Task extends React.Component {
             )}>
 
             <Row className={classnames(taskStyles.row, {[taskStyles.row__slideOut]: slideOut})} noMargin>
+              {dragHandle(provided)}
               <div className={taskStyles.status} onClick={this.handleCheck} >
                 <CheckMarkSVG className={taskStyles.status__checkmark}/>
               </div>
