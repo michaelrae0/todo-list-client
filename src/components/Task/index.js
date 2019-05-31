3fb46e2553b910633c1d78e4db29aa00861f7dc6
mdrae <mdrae0@gmail.com>
2019-05-31 14:10:59 -0500

    Adds ability to add tasks
    This commit introduces an `addItem` action/reducer that creates a new
    state with the original list, but with an item added.
    
    This commit also adds a presentational component that allows users to add
    tasks, complete with animations.

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -5,6 +5,7 @@ import { Draggable } from 'react-beautiful-dnd'
 import * as taskStyles from './task.module.scss'
 import Row from '../Row'
 import { ReactComponent as CheckMarkSVG } from '../../images/checkmark-black.svg'
+import { randomId } from '../../utils/strings'
 
 //input is h3
 class Task extends React.Component {
@@ -12,27 +13,43 @@ class Task extends React.Component {
     super(props);
 
     this.state = {
-      value: this.props.task.content,
+      value: this.props.init ? '' : this.props.task.content,
       slideOut: false,
     }
   }
 
+  componentDidMount() {
+    if (this.props.init) this.addedItem.focus(); // automatically focus added items
+  }
+
   handleCheck = () => {
-    const { index, handleClick, handleBorderFade } = this.props
+    const { index, handleComplete, handleBorderFade } = this.props
 
     this.setState({ slideOut: true })         // slide task off list
     handleBorderFade(index)                   // fade top border if task is at bottom of list
-    setTimeout(() => handleClick(index), 400) // delete from list
+    setTimeout(() => handleComplete(index), 400) // delete from list
+  }
+
+  handleBlur = (e, naturalBlur = false) => {
+    const { init, toggleAdd, addTask } = this.props
+    const { value } = this.state
+
+    if (naturalBlur || e.which === 13) { // Blur or enter
+      if (init) {
+        if (value) addTask(value)
+        toggleAdd()
+      }
+    }
   }
 
   render () {
-    const { task, index, bordered, slideUp, fadeBorder } = this.props
+    const { task, index, bordered, slideUp, slideDown, fadeBorder, init } = this.props
     const { value, slideOut } = this.state
 
-    const dragHandle = (provided) => { // self executes
+    const DragHandle = ({ provided }) => { // self executes
       let arr = Array(6).fill('')
       return (
-        <div className={taskStyles.handle} {...provided.dragHandleProps}>
+        <div className={classnames(taskStyles.handle, {[taskStyles.handle__hide]: init})} {...provided.dragHandleProps}>
           {arr.map((elem, i) => <div className={taskStyles.handle__dimple} key={i} />)}
         </div>
       )
@@ -40,7 +57,7 @@ class Task extends React.Component {
 
     // wrapper handles opacity transform separately from row's translateX
     return (
-      <Draggable draggableId={task.id} index={index}>
+      <Draggable draggableId={task ? task.id : `task-${randomId(10)}`} index={index}>
         {(provided, snapshot) => (
 
           <div 
@@ -49,19 +66,22 @@ class Task extends React.Component {
               taskStyles.wrapper, 
               {[taskStyles.wrapper__slideOut]: slideOut}, 
               {[taskStyles.wrapper__slideUp]: slideUp},
+              {[taskStyles.wrapper__slideDown]: slideDown},
               {[taskStyles.wrapper__isDragging]: snapshot.isDragging},
             )}>
 
             <Row className={classnames(taskStyles.row, {[taskStyles.row__slideOut]: slideOut})} noMargin>
-              {dragHandle(provided)}
+              <DragHandle provided={provided} />
               <div className={taskStyles.status} onClick={this.handleCheck} >
                 <CheckMarkSVG className={taskStyles.status__checkmark}/>
               </div>
               <input 
                 type='text' placeholder='Enter a Task'
+                ref={(input) => { this.addedItem = input; }} 
                 className={taskStyles.input}
                 value={value}
                 onChange={e => this.setState({ value: e.target.value })}
+                onBlur={e => this.handleBlur(e, true)} onKeyPress={e => this.handleBlur(e)}
               />
             </Row>
             {bordered && <div className={classnames(taskStyles.border, {[taskStyles.border__fadeOut]: fadeBorder})}/>}
